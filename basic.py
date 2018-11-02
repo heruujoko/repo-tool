cf650d856bd701f835e66cca5bceab8fbafcf62d
Heru <heru@wallextech.com>
2018-11-02 21:15:40 +0800

    tagger

---
+++ basic.py
@@ -0,0 +1,9 @@
+import urllib, json
+import subprocess
+
+# url = "https://jsonplaceholder.typicode.com/posts"
+# response = urllib.urlopen(url)
+# data = json.loads(response.read())
+# print data[0]['body']
+
+# tag the commit
